65e6db24ae0bae5023b1994836f6d6f3771159b6
sabrynadev <sabrynxr@gmail.com>
2026-08-17 12:18:45 -0300

    refactor: remove dead template code and nested legacy copy

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -105,7 +105,6 @@ export default function RootLayout() {
                 ) : (
                   <Stack.Screen name="index" options={{ headerShown: false }} />
                 )}
-                <Stack.Screen name="modal" options={{ presentation: "modal", title: "Modal" }} />
               </Stack>
             )}
             <StatusBar style="auto" />
